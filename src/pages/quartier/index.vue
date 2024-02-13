3cfe1e9f505033086aac4c94935cfb74c871caca
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-02-13 16:06:37 +0100

    AJout  groupby

--- src/pages/quartier/index.vue
+++ src/pages/quartier/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import groupBy from 'lodash/groupBy'
 import { supabase } from '../../supabase.ts'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 const { data, error } = await supabase.from('quartiercommune').select('*')
 console.log(data)
 if (error) console.log("n'a pas pu charger la table quartiercommune :", error)
@@ -14,5 +16,20 @@ if (error) console.log("n'a pas pu charger la table quartiercommune :", error)
         {{ quartierObject.nomQuartier }}
       </li>
     </ul>
+  </section>
+  <section class="flex flex-col">
+    <h3 class="text-2xl">Liste des quartiers</h3>
+    <div v-for="(listeQuartier, nomCommune) in groupBy(data, 'nomCommune')" :key="nomCommune">
+      <Disclosure>
+        <DisclosureButton>{{ nomCommune }}</DisclosureButton>
+        <DisclosurePanel>
+          <ul>
+            <li v-for="quartierObject in listeQuartier" :key="quartierObject.id_Quartier">
+              {{ quartierObject.nomQuartier }}
+            </li>
+          </ul>
+        </DisclosurePanel>
+      </Disclosure>
+    </div>
   </section>
 </template>
