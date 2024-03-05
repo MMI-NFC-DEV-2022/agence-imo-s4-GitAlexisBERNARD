3e9585d6c2ae3fe6a63e1b6e94b2f2494e28d746
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-03-05 14:20:59 +0100

    creation des pages svg

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <h1>Supabase Configurateur</h1>
+  <RouterLink to="/configurateur">
+  <button class="bg-slate-500 rounded-2xl">Lien vers page configurateur SVG</button>
+  </RouterLink>
 </template>
