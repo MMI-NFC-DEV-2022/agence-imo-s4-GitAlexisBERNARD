8925a27abec0abaca12eb0050e596c79137c4372
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-03-05 13:56:33 +0100

    initial commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,18 +7,5 @@ import { supabase } from '@/supabase'
 const { data: { user } } = await supabase.auth.getUser()
 </script>
 <template>
-  <h1>Supabase Configurateur</h1>
-  <RouterLink to="/basket/new">
-    <button class="bg-slate-500 rounded-2xl">Lien vers page configuration d'une Basket</button>
-  </RouterLink>
-  <section v-if="user">
-    <div>
-      <p>Affiche listes de basket de l'utilisateur avec SupaBase</p>
-      <ListeBaskets :max="3" />
-    </div>
-  </section>
-  <section v-else>
-    <p>Afiche la liste de toutes les basket faites par les utilisatuers</p>
-    <ListeAllBaskets />
-  </section>
+  <h1>Supabase HomePage</h1>
 </template>
