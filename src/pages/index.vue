32c00bbbccd379e7ce01a41f33881f3718d6cdf0
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2024-03-06 14:08:24 +0100

    ajout limite de trois affiche des bakset

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,9 @@ import ListeBaskets from '@/components/ListeBaskets.vue'
     <button class="bg-slate-500 rounded-2xl">Lien vers page configuration d'une Basket</button>
   </RouterLink>
   <section>
-      <div>
-        <p>Affiche listes de basket avec SupaBase</p>
-        <ListeBaskets />
-      </div>
+    <div>
+      <p>Affiche listes de basket avec SupaBase</p>
+      <ListeBaskets :max="3" />
+    </div>
   </section>
 </template>
